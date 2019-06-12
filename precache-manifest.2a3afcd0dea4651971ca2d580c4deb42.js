87976e74f875a8cf9773227443f4bd8ea435bb9f
Julio Cesar Lima <juliocesarlima19@gmail.com>
2019-06-12 13:22:04 -0300

    Updates

---
+++ precache-manifest.2a3afcd0dea4651971ca2d580c4deb42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036cc593e0fd2d5e89c819537bfdd510",
+    "url": "/curriculo/index.html"
+  },
+  {
+    "revision": "43e9872d41ca7adcc67b",
+    "url": "/curriculo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/curriculo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "43e9872d41ca7adcc67b",
+    "url": "/curriculo/static/js/main.2670494c.chunk.js"
+  },
+  {
+    "revision": "0f0d129290c9eb361483",
+    "url": "/curriculo/static/js/runtime~main.b71702cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/curriculo/static/media/logo.5d5d9eef.svg"
+  }
+]);
