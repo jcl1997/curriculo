8d1da3694c8d362f901a88b31c6fbf3428009add
Julio Cesar Lima <juliocesarlima19@gmail.com>
2019-06-14 08:28:25 -0300

    feat(backend): Adiciona About

---
+++ src/js/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Follow extends Component {
+  render() {
+    //  style="width: 20rem;"
+    return (
+      <div className="card">
+        <div className="card-header card-header-text bg-primary">
+          <div className="card-text">
+            <h4 className="card-title text-white">Sobre mim</h4>
+          </div>
+        </div>
+        <div className="card-body">
+          <p className="card-text" />
+        </div>
+      </div>
+    );
+  }
+}
--- src/js/User.js
+++ src/js/User.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import Profile from './Profile';
 import Contact from './Contact';
+import About from './About';
 import Hobbies from './Hobbies';
 import Follow from './Follow';
 
 export default class User extends Component {
-    render() {
-        return (
-            <div>
-                <div className="p-2 mx-auto mt-1">
-                    <Profile />
-                </div>
-                <div className="p-2 mx-auto mt-1">
-                    <Contact />
-                </div>
-                <div className="p-2 mx-auto mt-1">
-                    <Hobbies />
-                </div>
-                <div className="p-2 mx-auto mt-1">
-                    <Follow />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="p-2 mx-auto mt-1">
+          <Profile />
+        </div>
+        <div className="p-2 mx-auto mt-1">
+          <Contact />
+        </div>
+        <div className="p-2 mx-auto mt-1">
+          <About />
+        </div>
+        <div className="p-2 mx-auto mt-1">
+          <Hobbies />
+        </div>
+        <div className="p-2 mx-auto mt-1">
+          <Follow />
+        </div>
+      </div>
+    );
+  }
 }
