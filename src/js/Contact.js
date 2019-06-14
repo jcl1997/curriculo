20d640f283a1092a2609c3748e23fe4c7aefa917
Júlio César Lima <juliocesarlima19@gmail.com>
2019-06-13 23:38:36 -0300

    feat(frontend): adiciona style no contato

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -5,11 +5,24 @@ export default class Contact extends Component {
     //  style="width: 20rem;"
     return (
       <div className="card">
+        <div className="card-header card-header-text bg-success">
+          <div className="card-text">
+          <h4 className="card-title text-white">Contatos</h4>
+          </div>
+        </div>
         <div className="card-body">
-          <h4 className="card-title">Contatos</h4>
-          <p className="card-text">juliocesarlima19@gmail.com</p>
-          <p className="card-text">3322-9846</p>
-          <p className="card-text">+55 (31)99803-5980</p>
+          <p className="card-text">
+            <i className="fas fa-envelope-open-text" />
+            &nbsp;juliocesarlima19@gmail.com
+          </p>
+          <p className="card-text">
+            <i className="fas fa-phone-alt" />
+            &nbsp;3322-9846
+          </p>
+          <p className="card-text">
+            <i className="fas fa-mobile-alt" />
+            &nbsp;+55 (31)99803-5980
+          </p>
         </div>
       </div>
     );
