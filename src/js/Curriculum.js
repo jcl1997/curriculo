9e8251ead4317bfe9c9760fb61a2ba713937d750
Julio Cesar Lima <juliocesarlima19@gmail.com>
2019-06-14 16:33:12 -0300

    feat(frontend): adiciona Skills

--- src/js/Curriculum.js
+++ src/js/Curriculum.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Perfil from './Perfil';
+import Skills from './Skills';
 
 export default class Curriculum extends Component {
   render() {
@@ -11,6 +12,9 @@ export default class Curriculum extends Component {
         </div>
         <div className="p-2 mx-auto mt-1">
           <Perfil />
+        </div>
+        <div className="p-2 mx-auto mt-1">
+          <Skills />
         </div>
       </div>
     );
---
+++ src/js/Skills.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">
+          <h1 className="card-title text-white">Habilidades</h1>
+        </div>
+        <div className="pt-5 card-body">
+          <ul className="list-group list-group-flush">
+            <il className="list-group-item">
+              <p className="mb-0 align-middle">
+                <i className="fab fa-html5 fa-2x align-middle" />&nbsp;HTML e&nbsp;
+                <i className="fab fa-css3-alt fa-2x align-middle" />&nbsp;CSS básico
+              </p>
+            </il>
+            <il className="list-group-item">
+              <p className="mb-0 align-middle">
+                <i className="fas fa-database fa-2x align-middle" />&nbsp;Mysql básico
+              </p>
+            </il>
+            <il className="list-group-item">
+              <p className="mb-0 align-middle">
+                <i className="fas fa-cogs fa-2x align-middle" />&nbsp;Lógica de programação
+              </p>
+            </il>
+            <il className="list-group-item">
+              <p className="mb-0 align-middle">
+                <i className="fab fa-js fa-2x align-middle" />&nbsp;Javascript básico
+              </p>
+            </il>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
